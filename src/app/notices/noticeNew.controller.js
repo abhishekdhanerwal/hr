b698366ded77ea890d9426191e337512e7510a18
AbhishekDatiot <abhishek@datiot.com>
2017-06-12 11:05:26 +0530

    notice ui changes

--- src/app/notices/noticeNew.controller.js
+++ src/app/notices/noticeNew.controller.js
@@ -35,6 +35,27 @@
       vm.notice.attachmentUrl = [];
     };
 
+     vm.filterAudienceTable = function() {
+      // Declare variables
+      var input, filter, table, tr, td, i;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      table = document.getElementById("myTable");
+      tr = table.getElementsByTagName("tr");
+
+      // Loop through all table rows, and hide those who don't match the search query
+      for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+          if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+      }
+    }
+
     vm.endDateValidation = function () {
       var currentDate = Date.now()
       if(currentDate > vm.notice.startDate)
@@ -57,7 +78,7 @@
           //   lastModified: 'desc' // initial sorting
           // }, // count per page
           filter: {
-            name: '' // initial filter
+            name: '' // initial filter,
           }
         }, {
           counts: [],
